2a4d66f734575e0bfa7ba074edadb55e858baa69
V3il3DOraCl3 <javadaaghajanloo@gmail.com>
2022-07-13 18:10:14 +0430

    add blog_p

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,8 +1,17 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from typing import Optional
 
 router = APIRouter(prefix='/blog', tags=['Blog'], )
 
 
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    nb_comments: int
+    published: Optional[bool]
+
+
 @router.post('/new', summary='post a blog')
-def creat_blog():
-    return {'message': 'blog created'}
+def creat_blog(blog: BlogModel):
+    return {'message': 'blog created', 'data': blog}
